03813e8ac0971ebb83985624de8458ec260b3a3d
AhmedMHassaan <ahmed.m.hassaan1998@gmail.com>
2025-05-08 22:01:28 +0300

    add nav destinations

--- app/src/main/java/com/hassaanapps/onyxdeliveryservice/MainActivity.kt
+++ app/src/main/java/com/hassaanapps/onyxdeliveryservice/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.hassaanapps.onyxdeliveryservice.ui.nav.NavigationStack
 import com.hassaanapps.onyxdeliveryservice.ui.theme.OnyxDeliveryServiceTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +14,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+
             OnyxDeliveryServiceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController: NavHostController = rememberNavController()
+                NavigationStack(navController)
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OnyxDeliveryServiceTheme {
-        Greeting("Android")
     }
 }
